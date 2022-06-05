6e009f65f02bc231a02eb3ddc9a56e021926e354
mimicatsu <mimicatsu5168@gmail.com>
2022-06-05 19:01:54 +0800

    HW4 in python

---
+++ anfis_matlab/HW4_bankruptcy.m
@@ -0,0 +1,19 @@
+% T = readtable("../bankruptcy_HW1_train.csv");
+% X = T(2:end,3:end);
+% y = T(2:end,2);
+% X = [X,y];
+% writetable(X, "data.dat")
+% 
+% T = readtable("../bankruptcy_HW1_test.csv");
+% X = T(2:end,3:end);
+% y = T(2:end,2);
+% X = [X,y];
+% writetable(X, "data_test.dat")
+
+
+
+%fuzzyLogicDesigner
+
+mam_fis = readfis('fuzzy_new.fis');
+sug_fis = convertToSugeno(mam_fis);
+neuroFuzzyDesigner
